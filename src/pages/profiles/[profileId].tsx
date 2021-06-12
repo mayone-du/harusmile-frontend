861856b9c936f37eba6c4edc3c074e9f9c89a2f1
mayone-du <cocomayo1201@icloud.com>
2021-06-13 08:38:55 +0900

    Add skeleton-loading to [profilId].tsx

--- src/pages/profiles/[profileId].tsx
+++ src/pages/profiles/[profileId].tsx
@@ -7,12 +7,31 @@ import { MEDIAFILE_API_ENDPOINT } from "src/utils/API_ENDPOINTS";
 const ProfileDetail: NextPage = () => {
   const router = useRouter();
 
-  const { data } = useGetProfileQuery({
+  const { data, loading: isLoading } = useGetProfileQuery({
     variables: { profileId: router.asPath.replace("/profiles/", "") },
   });
+
   return (
     <Layout metaTitle="profile detail">
       <h2 className="py-4 text-3xl text-center">Profile Detail</h2>
+
+      {isLoading && (
+        <div className="flex items-center p-4 border shadow-md">
+          {/* 左 */}
+          <div className="flex items-center w-1/2 border-r">
+            <div className="mx-6 w-32 h-32 bg-gray-100 rounded-full border animate-pulse"></div>
+            <div>
+              <div className="my-2 w-52 h-6 bg-gray-100 animate-pulse"></div>
+              <div className="my-2 w-52 h-6 bg-gray-100 animate-pulse"></div>
+            </div>
+          </div>
+          {/* 右 */}
+          <div className="flex flex-col justify-center w-1/2 h-16 font-bold text-center">
+            <h3 className="text-2xl">これまでに相談したセンパイの数</h3>
+          </div>
+        </div>
+      )}
+
       {data?.profile && (
         <div>
           <section>
