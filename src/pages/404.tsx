296c445eee419d2abb61ca02bda493e23d6e9125
mayone-du <cocomayo1201@icloud.com>
2021-06-22 17:17:48 +0900

    Add empty plans/index page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,6 +1,7 @@
+import type { NextPage } from "next";
 import { Layout } from "src/components/layouts/Layout";
 
-const NotFoundPage = () => {
+const NotFoundPage: NextPage = () => {
   return (
     <div>
       <Layout spHeaderTitle="404 Not Found" metaTitle="404 Not Found">
---
+++ src/pages/plans/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+
+const PlansIndex: NextPage = () => {
+  // プラン一覧の表示
+  return <div></div>;
+};
+
+export default PlansIndex;
