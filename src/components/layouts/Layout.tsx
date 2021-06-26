11faa0479e9c85feb44c1e19fefb70eff9a6bdd3
mayone-du <cocomayo1201@icloud.com>
2021-06-26 10:28:31 +0900

    Fix _document Layout metas

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -16,6 +16,15 @@ export const Layout: React.FC<Props> = memo((props) => {
     <div>
       <Head>
         <title>{props.metaTitle}</title>
+        <meta property="og:url" content="https://" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Test Title" />
+        <meta property="og:description" content="Test Description" />
+        <meta property="og:site_name" content="Test SiteName" />
+        <meta property="og:image" content="/images/sample-image.jpg" />
+
+        <meta name="twitter:card" content="Summary Card" />
+        {/* <meta name="twitter:site" content="@mayo1201blog" /> */}
       </Head>
       <Header spHeaderTitle={props.spHeaderTitle} profileImagePath={loginUserData.profileImage} />
       <main className="mx-2 md:mx-32">{props.children}</main>
