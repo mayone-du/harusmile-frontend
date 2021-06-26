a84c69e3e749412cc836a758d0d16a8c9568568e
mayone-du <cocomayo1201@icloud.com>
2021-06-26 10:51:28 +0900

    Add PWA settings

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
-module.exports = {
+const withPWA = require("next-pwa");
+module.exports = withPWA({
   env: {
     GRAPHQL_API_URL: process.env.GRAPHQL_API_URL,
     CLOUDINARY_URL: process.env.CLOUDINARY_URL,
   },
+  pwa: {
+    dest: "public", // swの出力ディレクトリ
+    // runtimeCaching: [],
+  },
   // future: {
   //   webpack5: false,
   // },
-};
+});
