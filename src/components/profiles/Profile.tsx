5d4ed1381eb4744ccb340f8a81cda068e5841ef3
mayone-du <cocomayo1201@icloud.com>
2021-07-31 17:04:29 +0900

    Add star rating ui

--- src/components/profiles/Profile.tsx
+++ src/components/profiles/Profile.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { Rating, RatingView } from "react-simple-star-rating";
 import { ProfileImageIcon } from "src/components/icons/ProfileImageIcon";
 import { RightSvg } from "src/components/icons/svgs/RightSvg";
 import { Stars } from "src/components/profiles/Stars";
@@ -51,7 +52,34 @@ export const Profile: React.VFC<Props> = (props) => {
             {/* レビュー */}
             <div className="flex justify-between items-center py-4 border-b border-gray-400">
               <div>
-                <Stars />
+                <RatingView
+                  className="flex items-center"
+                  ratingValue={
+                    Math.round(
+                      props.stars.reduce((prev: number, current: number) => {
+                        return prev + current / props.stars.length;
+                      }, 0) * 10,
+                    ) / 10
+                  } /* RatingView Props */
+                  emptyColor={"#cccccc"}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="md:w-8 md:h-8 w-6 h-6"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+                    />
+                  </svg>
+                </RatingView>
+
+                {/* <Stars /> */}
               </div>
               <div className="md:text-base text-xs">
                 {/* 配列の数字の平均を出し、少数第二位を四捨五入 */}
