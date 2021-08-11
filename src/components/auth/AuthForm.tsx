cdd49aedb7b0e44d632e121d9aecf32ba1dec2de
mayone-du <cocomayo1201@icloud.com>
2021-08-11 18:26:04 +0900

    Update AuthForm UI

--- src/components/auth/AuthForm.tsx
+++ src/components/auth/AuthForm.tsx
@@ -27,21 +27,31 @@ export const AuthForm: React.VFC<Props> = (props) => {
       await handleSignUp(e);
     }
   };
+
+  const labelClassName = "md:w-1/3 w-2/3 mx-auto block text-sm";
+  const inputClassName = "border focus:outline-none block md:w-1/3 w-2/3 p-2 rounded mb-2 mx-auto";
   return (
     <form onSubmit={handleAuth}>
       {/* ローディング */}
       {isLoading && <div>loading</div>}
+      {/* 新規登録画面だった場合 */}
       {props.pageContext === "signup" && (
         <div>
+          <label htmlFor="username" className={labelClassName}>
+            ユーザー名
+          </label>
           <input
+            id="username"
             type="text"
-            className="border focus:outline-none block md:w-1/3 w-2/3 p-2 rounded-sm mx-auto"
+            className={inputClassName}
             placeholder="ユーザーネーム"
             value={inputProfileName}
             onChange={handleProfileNameChange}
             autoComplete="name"
           />
-          <div className="flex items-center justify-center">
+          {/* 大学生か高校生で登録 */}
+          <p className={labelClassName}>身分情報</p>
+          <div className="flex items-center justify-center mb-2">
             <button
               type="button"
               onClick={handleSetCollegeStudent}
@@ -62,9 +72,13 @@ export const AuthForm: React.VFC<Props> = (props) => {
             </button>
           </div>
           <div>
+            <label htmlFor="schoolName" className={labelClassName}>
+              学校名
+            </label>
             <input
+              id="schoolName"
               type="text"
-              className="border focus:outline-none block md:w-1/3 w-2/3 p-2 rounded-sm mx-auto"
+              className={inputClassName}
               placeholder="通っている大学、高校名"
               value={inputSchoolName}
               onChange={handleSchoolNameChange}
@@ -73,10 +87,15 @@ export const AuthForm: React.VFC<Props> = (props) => {
         </div>
       )}
 
+      {/* ログイン時は以下のみ表示 */}
       <div>
+        <label htmlFor="email" className={labelClassName}>
+          メールアドレス
+        </label>
         <input
+          id="email"
           type="email"
-          className="border focus:outline-none block md:w-1/3 w-2/3 p-2 rounded-sm mx-auto"
+          className={inputClassName}
           placeholder="メールアドレス"
           value={inputEmail}
           onChange={handleEmailChange}
@@ -84,16 +103,20 @@ export const AuthForm: React.VFC<Props> = (props) => {
         />
       </div>
       <div>
+        <label htmlFor="password" className={labelClassName}>
+          パスワード
+        </label>
         <input
+          id="password"
           type="password"
-          className="border focus:outline-none block md:w-1/3 w-2/3 p-2 rounded-sm mx-auto"
+          className={inputClassName}
           placeholder="パスワード"
           value={inputPassword}
           onChange={handlePasswordChange}
           autoComplete="current-password"
         />
       </div>
-      <button className="border block md:w-1/6 w-1/3 p-2 rounded-sm my-4 mx-auto" type="submit">
+      <button className="border block md:w-1/6 w-1/3 p-2 rounded-3xl my-4 mx-auto" type="submit">
         {props.pageContext === "signin" ? "ログイン" : "新規登録"}
       </button>
     </form>
